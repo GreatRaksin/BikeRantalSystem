317076b77adb8a69bd1295b8c6b74f8232ae3bad
demidraksin <greatraksin@icloud.com>
2020-12-12 18:01:04 +0300

    Adding tests

---
+++ assertion_tests.py
@@ -0,0 +1,24 @@
+def circle(r):
+    pi = 3.14
+    res = pi * r * 2
+    return res
+
+
+def test_circle(result):
+    assert circle(5) == result, 'should be 78.5'
+
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, 'should be 6'
+    print(True)
+
+
+def test_sum_tuple():
+    assert sum((1, 2, 3)) == 6, 'should be 6'
+
+
+if __name__ == '__main__':
+    test_sum()
+    test_sum_tuple()
+    test_circle(78.5)
+    print('Everything is ok')
