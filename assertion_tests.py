ce836c684f73592842d839e46e9fe658769b88bc
demidraksin <greatraksin@icloud.com>
2020-12-12 18:26:29 +0300

    Adding tests with unittest

--- assertion_tests.py
+++ assertion_tests.py
@@ -3,25 +3,21 @@ import unittest
 
 def circle(r):
     pi = 3.14
-    res = pi * r * 2
+    res = pi * r ** 2
     return res
 
 
 class TestSum(unittest.TestCase):
 
     def test_circle(self):
-        assert circle(5) == 78.5, ''
+        self.assertEqual(circle(5), 78.5, 'should be 78.5')
 
     def test_sum(self):
-        assert sum([1, 2, 3]) == 6, 'should be 6'
-        print(True)
+        self.assertEqual(sum([1, 2, 3]), 6, 'should be 6')
 
     def test_sum_tuple(self):
-        assert sum((1, 2, 3)) == 6, 'should be 6'
+        self.assertEqual(sum((1, 2, 3)), 6, 'should be 6')
 
 
 if __name__ == '__main__':
-    test_sum()
-    test_sum_tuple()
-    test_circle(78.5)
-    print('Everything is ok')
+    unittest.main()
