38f733be3e8704630cec213d57bbf0dc33e46fa5
demidraksin <greatraksin@icloud.com>
2020-12-12 18:37:33 +0300

    Adding tests for bikeRentalSystem

---
+++ bike_test.py
@@ -0,0 +1,9 @@
+import unittest
+from datetime import datetime, timedelta
+from bikeRental import BikeRental, Customer
+
+
+class BikeRentalTest(unittest.TestCase):
+
+    def test_display_correct_stocks(self):
+        shop1 =
